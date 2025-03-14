671b1121319dd454aa2e1fc4ee8b964b67f21822
Ronald Espitia <ronald79@gmail.com>
2025-03-13 20:39:00 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let variable = 1;
+const miArray = [2, 5, 6, 8];
+const miArrayStr = ['Ander', 'Ronald', 'Andres'];
+//const miArrayStrD = ['Luis', 2000, False];
+//indices               0    1     2
+const miString = 'hola'
+const miString2 = 'Ander'
+//indices         0123456789
+let tamanio = miString.length;
+console.log(miString.slice(5, 10));
+console.log(miString + ' ' + miString2);
+// literal templates
+console.log(`${miString} Como estas ${miString2}`);
+
+console.log(miArray.length);
+const miArray2 = [];
+miArray.push(100, 200);
+console.log(miArray);
